3d3ba53cb06d44bb013c770a6c600d504bcedd11
AryanAmin1902 <amin5300@mylaurier.ca>
2023-11-09 11:51:44 -0500

    feature(server): Added all the environment variables to .env file and updated respective files

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const youtubeRoutes = require("./routes/youtube");
 const courseRoutes = require("./routes/course");
 const videoRoutes = require("./routes/video");
 const userRoutes = require("./routes/user");
 
-const username = encodeURIComponent("maestrodb");
-const password = encodeURIComponent("Maestro@123");
+const SERVER_PORT = process.env.SERVER_PORT;
+const username = encodeURIComponent(process.env.MONGO_USERNAME);
+const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 const uri = `mongodb+srv://${username}:${password}@maestro.dqc9ifa.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/images", express.static(path.join(__dirname,"images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -37,13 +38,13 @@ app.use("/api/video", videoRoutes);
 app.use("/api/user", userRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
 app.use((error, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path, err => {
+    fs.unlink(req.file.path, (err) => {
       console.log(err);
     });
   }
@@ -51,16 +52,16 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 mongoose
   .connect(uri)
   .then(() => {
-    app.listen(8000, function() {
-      console.log("Server is running on port " + 8000);
+    app.listen(SERVER_PORT, function () {
+      console.log("Server is running on port " + SERVER_PORT);
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
--- controllers/youtube.js
+++ controllers/youtube.js
@@ -1,8 +1,8 @@
 const { google } = require("googleapis");
-const axios = require('axios');
+const axios = require("axios");
 
-const apiKey = "AIzaSyCe1PYck7G8XvV-ujzwhgTqXzf6mjHCbgs";
-const apiUrl = "http://localhost:8000/api/"
+const apiKey = process.env.YOUTUBE_API_KEY;
+const apiUrl = process.env.API_URL;
 
 const youtube = google.youtube({
   version: "v3",
@@ -10,92 +10,96 @@ const youtube = google.youtube({
 });
 
 exports.getLink = (req, res, next) => {
-    res.status(201).json({
-        message: "Link added successfully",
-    });
+  res.status(201).json({
+    message: "Link added successfully",
+  });
 };
 
 exports.getSearch = async (req, res, next) => {
+  try {
+    const searchQuery = req.query.search_query;
+    const searchPlaylists = await youtube.search.list({
+      part: "snippet",
+      q: searchQuery,
+      order: "viewCount",
+      type: "playlist",
+    });
+
+    const searchPlaylist = searchPlaylists.data.items[0];
+
+    const courseData = {
+      title: searchPlaylist.snippet.title,
+      playlistId: searchPlaylist.id.playlistId,
+      publishedAt: searchPlaylist.snippet.publishedAt,
+      description: searchPlaylist.snippet.description,
+      imageUrl: searchPlaylist.snippet.thumbnails.high.url,
+    };
+
     try {
-        const searchQuery = req.query.search_query;
-        const searchPlaylists = await youtube.search.list({
-            part: "snippet",
-            q: searchQuery,
-            order: "viewCount",
-            type: "playlist"
+      const response = await axios.post(`${apiUrl}course`, courseData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const playlistId = courseData.playlistId;
+      const courseId = response.data.course.id;
+
+      let allPlaylistItems = [];
+      let nextPageToken = null;
+      let playlistItems;
+
+      do {
+        playlistItems = await youtube.playlistItems.list({
+          part: ["snippet", "contentDetails"],
+          playlistId: playlistId,
+          maxResults: 50,
+          pageToken: nextPageToken,
         });
+        allPlaylistItems = allPlaylistItems.concat(playlistItems.data.items);
+        nextPageToken = playlistItems.data.nextPageToken;
+      } while (nextPageToken);
 
-        const searchPlaylist = searchPlaylists.data.items[0];
+      let videoLinks = [];
 
-        const courseData = {
-            title: searchPlaylist.snippet.title,
-            playlistId: searchPlaylist.id.playlistId,
-            publishedAt: searchPlaylist.snippet.publishedAt,
-            description: searchPlaylist.snippet.description,
-            imageUrl: searchPlaylist.snippet.thumbnails.high.url,
-        };
+      await Promise.all(
+        allPlaylistItems.map(async (item) => {
+          const videoData = {
+            videoId: item.contentDetails.videoId,
+            title: item.snippet.title,
+            courseId: courseId,
+          };
 
-        try {
-            const response = await axios.post(`${apiUrl}course`, courseData, {
-                headers: {
-                  'Content-Type': 'application/json',
-                }
+          try {
+            await axios.post(`${apiUrl}video`, videoData, {
+              headers: {
+                "Content-Type": "application/json",
+              },
             });
 
-            const playlistId = courseData.playlistId;
-            const courseId = response.data.course.id;
+            videoLinks = videoLinks.concat(videoData);
+          } catch (error) {
+            console.error(
+              `Error posting video data for videoId ${videoData.videoId}:`,
+              error
+            );
+            return;
+          }
+        })
+      );
 
-            let allPlaylistItems = [];
-            let nextPageToken = null;
-            let playlistItems;
-
-            do {
-                playlistItems = await youtube.playlistItems.list({
-                    part: ["snippet", "contentDetails"],
-                    playlistId: playlistId,
-                    maxResults: 50,
-                    pageToken: nextPageToken
-                });
-                allPlaylistItems = allPlaylistItems.concat(playlistItems.data.items);
-                nextPageToken = playlistItems.data.nextPageToken;
-            } while (nextPageToken);
-
-            let videoLinks = []
-
-            await Promise.all(allPlaylistItems.map(async (item) => {
-                const videoData = {
-                    videoId: item.contentDetails.videoId,
-                    title: item.snippet.title,
-                    courseId: courseId,
-                };
-              
-                try {
-                    await axios.post(`${apiUrl}video`, videoData, {
-                        headers: {
-                        'Content-Type': 'application/json',
-                        },
-                    });
-              
-                    videoLinks = videoLinks.concat(videoData);
-                } catch (error) {
-                    console.error(`Error posting video data for videoId ${videoData.videoId}:`, error);
-                    return;
-                }
-            }));
-
-            res.json(videoLinks);
-        } catch (error) {
-            if (error.response) {
-                console.error('Response error:', error.response.data);
-                console.error('Status code:', error.response.status);
-            } else if (error.request) {
-                console.error('No response received:', error.request);
-            } else {
-                console.error('Request setup error:', error.message);
-            }
-        };
-
-    } catch (err) {
-        next(err);
+      res.json(videoLinks);
+    } catch (error) {
+      if (error.response) {
+        console.error("Response error:", error.response.data);
+        console.error("Status code:", error.response.status);
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+      } else {
+        console.error("Request setup error:", error.message);
+      }
     }
+  } catch (err) {
+    next(err);
+  }
 };
